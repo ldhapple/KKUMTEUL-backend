2501d62b523ca248ec8598461be3439f6dc17456
fbgjung <fbgjung@gmail.com>
2024-10-14 23:23:31 +0900

    Init: 프로젝트 세팅

---
+++ src/main/java/com/kkumteul/KkumteulApplication.java
@@ -0,0 +1,13 @@
+package com.kkumteul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkumteulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkumteulApplication.class, args);
+	}
+
+}
