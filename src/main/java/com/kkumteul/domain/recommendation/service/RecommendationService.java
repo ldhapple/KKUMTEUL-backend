6ee8a02723f3bd024971c5323440d9cb2efdcc21
leedohyun <lucky77795@naver.com>
2024-10-18 10:28:10 +0900

    Feat: RecommendationService 구현
    추천 도서를 조회하고, 해당 도서를 Dto로 변환하는 로직 구현

---
+++ src/main/java/com/kkumteul/domain/recommendation/service/RecommendationService.java
@@ -0,0 +1,25 @@
+package com.kkumteul.domain.recommendation.service;
+
+import com.kkumteul.domain.book.entity.Book;
+import com.kkumteul.domain.recommendation.dto.RecommendBookDto;
+import com.kkumteul.domain.recommendation.repository.RecommendationRepository;
+import com.kkumteul.exception.BookNotFoundException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RecommendationService {
+
+    private final RecommendationRepository recommendationRepository;
+
+    public List<RecommendBookDto> getRecommendedBooks(Long childProfileId) {
+        List<Book> recommendBooks = recommendationRepository.findBookByChildProfileId(childProfileId)
+                .orElseThrow(() -> new BookNotFoundException(childProfileId));
+
+        return recommendBooks.stream()
+                .map(RecommendBookDto::fromEntity)
+                .toList();
+    }
+}
