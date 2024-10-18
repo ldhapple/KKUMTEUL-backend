3e4853b43f172233eb030c55d602d687b1a07b9f
MINAH <tjdns7831@naver.com>
2024-10-18 17:38:07 +0900

    chore: Dto 통일 ( GetBookListResponseDto )

--- src/main/java/com/kkumteul/domain/book/dto/GetBookListResponseDto.java
+++ src/main/java/com/kkumteul/domain/book/dto/GetBookListResponseDto.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @Getter
 @Builder
-public class GetBookListResponse {
+public class GetBookListResponseDto {
     private String title;
     private byte[] bookImage; // 또는 String으로 바꿔서 이미지 URL을 저장할 수도 있습니다.
     private List<String> topics; // Topic 이름 리스트
