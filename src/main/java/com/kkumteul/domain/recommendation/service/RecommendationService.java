f4d1cae3c6a85e2cdd9f28c76dcdbffa9350f639
leedohyun <lucky77795@naver.com>
2024-10-18 13:56:07 +0900

    Chore: Service 코드에 로그 추가
    Service 코드에 로그를 추가 함.

--- src/main/java/com/kkumteul/domain/recommendation/service/RecommendationService.java
+++ src/main/java/com/kkumteul/domain/recommendation/service/RecommendationService.java
@@ -6,18 +6,22 @@ import com.kkumteul.domain.recommendation.repository.RecommendationRepository;
 import com.kkumteul.exception.RecommendationBookNotFoundException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class RecommendationService {
 
     private final RecommendationRepository recommendationRepository;
 
     public List<RecommendBookDto> getRecommendedBooks(Long childProfileId) {
+        log.info("getRecommendedBooks - Input childProfileId: {}", childProfileId);
         List<Book> recommendBooks = recommendationRepository.findBookByChildProfileId(childProfileId)
                 .orElseThrow(() -> new RecommendationBookNotFoundException(childProfileId));
 
+        log.info("found recommendedBooks: {}", recommendBooks.size());
         return recommendBooks.stream()
                 .map(RecommendBookDto::fromEntity)
                 .toList();
