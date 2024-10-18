092b03a12d4fcb7e080f892f3e1753a12b5f62b7
leedohyun <lucky77795@naver.com>
2024-10-18 16:11:04 +0900

    Chore: ChildProfileNotFOundException 메시지 수정
    조금 더 넓은 범위로 사용할 수 있도록 메시지 수정

--- src/main/java/com/kkumteul/exception/ChildProfileNotFoundException.java
+++ src/main/java/com/kkumteul/exception/ChildProfileNotFoundException.java
@@ -2,6 +2,6 @@ package com.kkumteul.exception;
 
 public class ChildProfileNotFoundException extends RuntimeException {
     public ChildProfileNotFoundException(Long input) {
-        super(String.format("해당 유저의 자녀 프로필을 찾을 수 없습니다. InputUserId: %s", input));
+        super(String.format("자녀 프로필을 찾을 수 없습니다. InputUserId: %s", input));
     }
 }
