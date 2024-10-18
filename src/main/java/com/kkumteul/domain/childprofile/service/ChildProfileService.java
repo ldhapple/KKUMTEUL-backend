6032e993289c1beaf709e5fbf88df748c2f7bed0
leedohyun <lucky77795@naver.com>
2024-10-18 14:36:48 +0900

    Feat: ChildProfileService 구현
    자녀 프로필 조회 서비스 로직 구현

---
+++ src/main/java/com/kkumteul/domain/childprofile/service/ChildProfileService.java
@@ -0,0 +1,29 @@
+package com.kkumteul.domain.childprofile.service;
+
+import com.kkumteul.domain.childprofile.dto.ChildProfileDto;
+import com.kkumteul.domain.childprofile.entity.ChildProfile;
+import com.kkumteul.domain.childprofile.repository.ChildProfileRepository;
+import com.kkumteul.exception.ChildProfileNotFoundException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ChildProfileService {
+
+    private final ChildProfileRepository childProfileRepository;
+
+    public List<ChildProfileDto> getChildProfile(Long userId) {
+        log.info("getChildProfiles - Input userId: {}", userId);
+        List<ChildProfile> childProfiles = childProfileRepository.findByUserId(userId)
+                .orElseThrow(() -> new ChildProfileNotFoundException(userId));
+
+        log.info("found childProfiles: {}", childProfiles.size());
+        return childProfiles.stream()
+                .map(ChildProfileDto::fromEntity)
+                .toList();
+    }
+}
