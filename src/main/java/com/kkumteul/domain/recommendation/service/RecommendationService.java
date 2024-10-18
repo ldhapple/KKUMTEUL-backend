9cf617ec13310412f34c5ff5ae4453615f6cbbc2
leedohyun <lucky77795@naver.com>
2024-10-18 10:29:05 +0900

    Feat: RecommendationBookNotFoundException 구현
    해당 프로필의 추천 도서를 조회하지 못했을 경우의 예외 구현

--- src/main/java/com/kkumteul/domain/recommendation/service/RecommendationService.java
+++ src/main/java/com/kkumteul/domain/recommendation/service/RecommendationService.java
@@ -3,7 +3,7 @@ package com.kkumteul.domain.recommendation.service;
 import com.kkumteul.domain.book.entity.Book;
 import com.kkumteul.domain.recommendation.dto.RecommendBookDto;
 import com.kkumteul.domain.recommendation.repository.RecommendationRepository;
-import com.kkumteul.exception.BookNotFoundException;
+import com.kkumteul.exception.RecommendationBookNotFoundException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,7 +16,7 @@ public class RecommendationService {
 
     public List<RecommendBookDto> getRecommendedBooks(Long childProfileId) {
         List<Book> recommendBooks = recommendationRepository.findBookByChildProfileId(childProfileId)
-                .orElseThrow(() -> new BookNotFoundException(childProfileId));
+                .orElseThrow(() -> new RecommendationBookNotFoundException(childProfileId));
 
         return recommendBooks.stream()
                 .map(RecommendBookDto::fromEntity)
