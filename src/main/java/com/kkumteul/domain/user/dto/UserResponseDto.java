116fd8f1b0f8b15220364545c0a4ff3737570dff
fbgjung <fbgjung@gmail.com>
2024-10-19 23:51:15 +0900

    Feat: UserResponseDto에 자녀 프로필 리스트 추가
    유저 조회 데이터에 자녀 프로필 정보 포함

--- src/main/java/com/kkumteul/domain/user/dto/UserResponseDto.java
+++ src/main/java/com/kkumteul/domain/user/dto/UserResponseDto.java
@@ -1,9 +1,11 @@
 package com.kkumteul.domain.user.dto;
 
+import com.kkumteul.domain.childprofile.dto.ChildProfileDto;
 import com.kkumteul.domain.user.entity.User;
 import lombok.*;
 
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @ToString
@@ -16,15 +18,21 @@ public class UserResponseDto {
     private String nickName;
     private String phoneNumber;
     private Date birthDate;
+    private List<ChildProfileDto> childProfileList;
 
     public static UserResponseDto fromEntity(User user) {
+        List<ChildProfileDto> childProfiles = user.getChildProfileList().stream()
+                .map(ChildProfileDto::fromEntity)
+                .toList();
+
         return new UserResponseDto(
-            user.getId(),
-            user.getUsername(),
-            user.getProfileImage(),
-            user.getNickName(),
-            user.getPhoneNumber(),
-            user.getBirthDate()
+                user.getId(),
+                user.getUsername(),
+                user.getProfileImage(),
+                user.getNickName(),
+                user.getPhoneNumber(),
+                user.getBirthDate(),
+                childProfiles
         );
     }
 }
