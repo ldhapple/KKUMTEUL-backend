f1e176133944345ea2f97551227f52b7863b7f53
fbgjung <fbgjung@gmail.com>
2024-10-19 23:52:46 +0900

    Feat: 자녀 프로필을 포함한 유저 조회 쿼리 추가

--- src/main/java/com/kkumteul/domain/user/repository/UserRepository.java
+++ src/main/java/com/kkumteul/domain/user/repository/UserRepository.java
@@ -2,6 +2,14 @@ package com.kkumteul.domain.user.repository;
 
 import com.kkumteul.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.childProfileList WHERE u.id = :userId")
+    Optional<User> findByIdWithChildProfiles(Long userId);
 }
