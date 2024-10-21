95adb01cad0b78dd03b768efc82e2c6a0a1f0727
xnfnfnr <hjs000914@gmail.com>
2024-10-21 17:39:36 +0900

    Feat: BookMBTIService 구현

--- src/main/java/com/kkumteul/domain/book/service/BookMBTIService.java
+++ src/main/java/com/kkumteul/domain/book/service/BookMBTIService.java
@@ -1,4 +1,20 @@
 package com.kkumteul.domain.book.service;
 
-public class BookMbtiService {
+import com.kkumteul.domain.book.entity.BookMBTI;
+import com.kkumteul.domain.book.repository.BookMbtiRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class BookMBTIService {
+    private BookMbtiRepository bookMbtiRepository;
+
+    public BookMBTI insertBookMBTI(BookMBTI bookMBTI) {
+        return bookMbtiRepository.save(bookMBTI);
+    }
 }
