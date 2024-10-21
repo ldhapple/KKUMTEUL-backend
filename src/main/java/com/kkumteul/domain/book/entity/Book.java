6395577b652f58b707d8bbf066027d7b86b20577
xnfnfnr <hjs000914@gmail.com>
2024-10-21 13:51:56 +0900

    Feat: Book엔티티 내에 ageGroup 변수 추가

--- src/main/java/com/kkumteul/domain/book/entity/Book.java
+++ src/main/java/com/kkumteul/domain/book/entity/Book.java
@@ -26,6 +26,7 @@ public class Book {
     private String publisher;
     private String price;
     private String page;
+    private String ageGroup;
 
     @Column(columnDefinition = "TEXT")
     private String summary;
@@ -36,13 +37,14 @@ public class Book {
 
     @Builder
     public Book(String title, String author, String publisher, String price, String page, String summary,
-                byte[] bookImage) {
+                String ageGroup, byte[] bookImage) {
         this.title = title;
         this.author = author;
         this.publisher = publisher;
         this.price = price;
         this.page = page;
         this.summary = summary;
+        this.ageGroup = ageGroup;
         this.bookImage = bookImage;
     }
 }
