6f96dc6df480b3b7fe43b0518718966c05c93a01
xnfnfnr <hjs000914@gmail.com>
2024-10-21 17:39:52 +0900

    Feat: BookTopicService 구현

--- src/main/java/com/kkumteul/domain/book/service/BookTopicService.java
+++ src/main/java/com/kkumteul/domain/book/service/BookTopicService.java
@@ -1,4 +1,21 @@
 package com.kkumteul.domain.book.service;
 
+import com.kkumteul.domain.book.entity.BookTopic;
+import com.kkumteul.domain.book.repository.BookTopicRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class BookTopicService {
+    private BookTopicRepository bookTopicRepository;
+
+    public BookTopic save(BookTopic bookTopic) {
+        return bookTopicRepository.save(bookTopic);
+    }
 }
