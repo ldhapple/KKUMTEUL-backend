6ec694ccbfaa576e692dc3910c3a6d9cdafd2b1f
xnfnfnr <hjs000914@gmail.com>
2024-10-21 17:38:21 +0900

    Feat: BookService 구현

--- src/main/java/com/kkumteul/domain/book/service/BookService.java
+++ src/main/java/com/kkumteul/domain/book/service/BookService.java
@@ -1,4 +1,26 @@
 package com.kkumteul.domain.book.service;
 
+import com.kkumteul.domain.book.dto.BookDto;
+import com.kkumteul.domain.book.entity.Book;
+import com.kkumteul.domain.book.repository.BookRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class BookService {
+    private BookRepository bookRepository;
+
+    // 1. 도서 등록
+    public BookDto insertBook(Book book) {
+
+        Book insertedBook = bookRepository.save(book);
+        BookDto insertedBookDto = BookDto.fromEntity(insertedBook);
+
+        return insertedBookDto;
+    }
 }
