f6b3c039ba8be94bc6e5d4ea6f196c7783f04ccb
xnfnfnr <hjs000914@gmail.com>
2024-10-22 15:44:55 +0900

    Refactor: AdminBookController에 있는 비즈니스 로직 코드를 BookService 로 이동

--- src/main/java/com/kkumteul/domain/book/controller/AdminBookController.java
+++ src/main/java/com/kkumteul/domain/book/controller/AdminBookController.java
@@ -1,21 +1,7 @@
 package com.kkumteul.domain.book.controller;
 
 import com.kkumteul.domain.book.dto.AdminInsertBookRequestDto;
-import com.kkumteul.domain.book.dto.BookDto;
-import com.kkumteul.domain.book.entity.Book;
-import com.kkumteul.domain.book.entity.BookGenre;
-import com.kkumteul.domain.book.entity.BookMBTI;
-import com.kkumteul.domain.book.entity.BookTopic;
-import com.kkumteul.domain.book.service.BookGenreService;
-import com.kkumteul.domain.book.service.BookMBTIService;
 import com.kkumteul.domain.book.service.BookService;
-import com.kkumteul.domain.book.service.BookTopicService;
-import com.kkumteul.domain.mbti.entity.MBTI;
-import com.kkumteul.domain.mbti.service.MBTIService;
-import com.kkumteul.domain.personality.entity.Genre;
-import com.kkumteul.domain.personality.entity.Topic;
-import com.kkumteul.domain.personality.service.GenreService;
-import com.kkumteul.domain.personality.service.TopicService;
 import com.kkumteul.util.ApiUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -32,76 +18,11 @@ import static com.kkumteul.util.ApiUtil.ApiSuccess;
 public class AdminBookController {
 
     private final BookService bookService;
-    private final BookGenreService bookGenreService;
-    private final BookMBTIService bookMBTIService;
-    private final BookTopicService bookTopicService;
-
-    private final GenreService genreService;
-    private final MBTIService mbtiService;
-    private final TopicService topicService;
 
     // 1. 도서 등록
     @PostMapping
     public ApiSuccess<?> insertBook(@RequestBody AdminInsertBookRequestDto adminInsertBookRequestDto) {
-        // 1.1. Book 엔티티 생성 및 저장
-        Book book = new Book(
-                adminInsertBookRequestDto.getTitle(),
-                adminInsertBookRequestDto.getAuthor(),
-                adminInsertBookRequestDto.getPublisher(),
-                adminInsertBookRequestDto.getPrice(),
-                adminInsertBookRequestDto.getPage(),
-                adminInsertBookRequestDto.getSummary(),
-                adminInsertBookRequestDto.getAgeGroup(),
-                adminInsertBookRequestDto.getBookImage()
-        );
-
-        // 1.2. 도서 등록
-        bookService.insertBook(book);
-
-        // 도서 수정할 때 필요한 코드
-//        // 1.2. 도서 등록
-//        BookDto savedBook = bookService.insertBook(book);
-//        // 1.3. 저장된 도서의 ID 가져오기
-//        Long savedBookId = bookService.getBookById(savedBook.getId()).getId();
-
-        // 1.4. BookGenre 엔티티 생성 및 저장
-        for (Genre genre : adminInsertBookRequestDto.getBookGenreList()) {
-            // bookGenre 등록 전, Genre 먼저 등록
-            Genre savedGenre = genreService.insertGenre(genre);
-
-            // bookGenre 등록
-            BookGenre bookGenre = BookGenre.builder()
-                    .book(book)
-                    .genre(savedGenre)
-                    .build();
-            bookGenreService.insertBookGenre(bookGenre);
-        }
-
-        // 1.5. BookMbti 엔티티 생성 및 저장
-        {
-            // BookMbti 등록 전, MBTI 먼저 등록
-            MBTI savedMBTI = mbtiService.insertMBTI(adminInsertBookRequestDto.getBookMbti());
-
-            // BookMbti 등록
-            BookMBTI bookMbti = BookMBTI.builder()
-                    .book(book)
-                    .mbti(savedMBTI)
-                    .build();
-            bookMBTIService.insertBookMBTI(bookMbti);
-        }
-
-        // 1.6. BookTopic 엔티티 생성 및 저장
-        for (Topic topic : adminInsertBookRequestDto.getBookTopicList()) {
-            // BookTopic 등록 전, Topic 먼저 등록
-            Topic savedTopic = topicService.insertTopic(topic);
-
-            // BookTopic 등록
-            BookTopic bookTopic = BookTopic.builder()
-                    .book(book)
-                    .topic(savedTopic)
-                    .build();
-            bookTopicService.insertBookTopic(bookTopic);
-        }
+        bookService.insertBook(adminInsertBookRequestDto);
 
         return ApiUtil.success("book insert successfully");
     }
--- src/main/java/com/kkumteul/domain/book/service/BookService.java
+++ src/main/java/com/kkumteul/domain/book/service/BookService.java
@@ -1,11 +1,20 @@
 package com.kkumteul.domain.book.service;
 
+import com.kkumteul.domain.book.dto.AdminInsertBookRequestDto;
 import com.kkumteul.domain.book.dto.BookDto;
 import com.kkumteul.domain.book.entity.Book;
+import com.kkumteul.domain.book.entity.BookGenre;
+import com.kkumteul.domain.book.entity.BookMBTI;
+import com.kkumteul.domain.book.entity.BookTopic;
 import com.kkumteul.domain.book.repository.BookRepository;
+import com.kkumteul.domain.mbti.entity.MBTI;
+import com.kkumteul.domain.mbti.service.MBTIService;
+import com.kkumteul.domain.personality.entity.Genre;
+import com.kkumteul.domain.personality.entity.Topic;
+import com.kkumteul.domain.personality.service.GenreService;
+import com.kkumteul.domain.personality.service.TopicService;
 import com.kkumteul.exception.BookNotFoundException;
 import jakarta.transaction.Transactional;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,14 +25,68 @@ import org.springframework.stereotype.Service;
 @Transactional
 public class BookService {
     private final BookRepository bookRepository;
+    private final BookGenreService bookGenreService;
+    private final BookMBTIService bookMBTIService;
+    private final BookTopicService bookTopicService;
+
+    private final GenreService genreService;
+    private final MBTIService mbtiService;
+    private final TopicService topicService;
 
     // 1. 도서 등록
-    public BookDto insertBook(Book book) {
+    public BookDto insertBook(AdminInsertBookRequestDto adminInsertBookRequestDto){
+        // 1.1. Book 엔티티 생성 및 저장
+        Book book = Book.builder()
+                .title(adminInsertBookRequestDto.getTitle())
+                .author(adminInsertBookRequestDto.getAuthor())
+                .publisher(adminInsertBookRequestDto.getPublisher())
+                .price(adminInsertBookRequestDto.getPrice())
+                .page(adminInsertBookRequestDto.getPage())
+                .summary(adminInsertBookRequestDto.getSummary())
+                .ageGroup(adminInsertBookRequestDto.getAgeGroup())
+                .bookImage(adminInsertBookRequestDto.getBookImage())
+                .build();
 
-        Book insertedBook = bookRepository.save(book);
-        BookDto insertedBookDto = BookDto.fromEntity(insertedBook);
+        // 1.2. 도서 등록
+        Book savedBook = bookRepository.save(book);
 
-        return insertedBookDto;
+        // 1.3. BookGenre 엔티티 생성 및 저장
+        for (Genre genre : adminInsertBookRequestDto.getBookGenreList()) {
+            // bookGenre 등록 전, Genre 먼저 등록
+            Genre savedGenre = genreService.insertGenre(genre);
+            // bookGenre 등록
+            BookGenre bookGenre = BookGenre.builder()
+                    .book(savedBook)
+                    .genre(savedGenre)
+                    .build();
+            bookGenreService.insertBookGenre(bookGenre);
+        }
+
+        // 1.4. BookMbti 엔티티 생성 및 저장
+        {
+            // BookMbti 등록 전, MBTI 먼저 등록
+            MBTI savedMBTI = mbtiService.insertMBTI(adminInsertBookRequestDto.getBookMbti());
+            // BookMbti 등록
+            BookMBTI bookMbti = BookMBTI.builder()
+                    .book(savedBook)
+                    .mbti(savedMBTI)
+                    .build();
+            bookMBTIService.insertBookMBTI(bookMbti);
+        }
+
+        // 1.5. BookTopic 엔티티 생성 및 저장
+        for (Topic topic : adminInsertBookRequestDto.getBookTopicList()) {
+            // BookTopic 등록 전, Topic 먼저 등록
+            Topic savedTopic = topicService.insertTopic(topic);
+            // BookTopic 등록
+            BookTopic bookTopic = BookTopic.builder()
+                    .book(savedBook)
+                    .topic(savedTopic)
+                    .build();
+            bookTopicService.insertBookTopic(bookTopic);
+        }
+
+        return BookDto.fromEntity(savedBook);
     }
 
     // 2. 도서 조회
