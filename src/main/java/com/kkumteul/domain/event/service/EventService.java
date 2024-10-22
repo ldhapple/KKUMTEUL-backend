d253464ed804da4ca61485152fae077b0149236c
fbgjung <fbgjung@gmail.com>
2024-10-22 19:10:29 +0900

    EventService에 insertJoinEvent 메서드 구현
    이벤트 참여 로직 추가

---
+++ src/main/java/com/kkumteul/domain/event/service/EventService.java
@@ -0,0 +1,63 @@
+package com.kkumteul.domain.event.service;
+
+import com.kkumteul.domain.event.dto.EventRequestDto;
+import com.kkumteul.domain.event.entity.Event;
+import com.kkumteul.domain.event.entity.JoinEvent;
+import com.kkumteul.domain.event.repository.EventRepository;
+import com.kkumteul.domain.event.repository.JoinEventRepository;
+import com.kkumteul.domain.user.entity.User;
+import com.kkumteul.domain.user.respository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EventService {
+
+    private final JoinEventRepository joinEventRepository;
+    private final UserRepository userRepository;
+    private final EventRepository eventRepository;
+
+    public void insertJoinEvent(Long userId, EventRequestDto eventRequestDto) {
+        log.info("user id: {}", userId);
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("user not found: " + userId));
+
+        Event event = eventRepository.findById(eventRequestDto.getEventId())
+                .orElseThrow(() -> new IllegalArgumentException("event not found: " + userId));
+
+        boolean isValidUser = isValidUser(user, eventRequestDto);
+        log.info("Is valid user: {}", isValidUser);
+        if(!isValidUser) {
+            throw new IllegalArgumentException("입력한 정보가 회원 정보와 일치한지 확인해주세요.");
+        }
+
+        boolean hasUserJoined = joinEventRepository.existsByUserAndEvent(user, event);
+        log.info("Has user joined: {}", hasUserJoined);
+        if(hasUserJoined) {
+            throw new IllegalArgumentException("이미 참여한 이벤트입니다.");
+        }
+
+        JoinEvent joinEvent = JoinEvent.builder()
+                .user(user)
+                .name(eventRequestDto.getName())
+                .phoneNumber(eventRequestDto.getPhoneNumber())
+                .createdAt(LocalDateTime.now())
+                .event(event)
+                .isWin(false)
+                .build();
+
+        JoinEvent savedJoinEvent = joinEventRepository.save(joinEvent);
+        log.info("Check saved join event user name: {}", savedJoinEvent.getUser().getUsername());
+
+    }
+
+    private boolean isValidUser(User user, EventRequestDto eventRequestDto) {
+        return user.getUsername().equals(eventRequestDto.getName()) && user.getPhoneNumber().equals(eventRequestDto.getPhoneNumber());
+    }
+
+}
