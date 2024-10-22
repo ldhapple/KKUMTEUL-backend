2982ca99cf9537863468307490f0167794f18034
fbgjung <fbgjung@gmail.com>
2024-10-22 19:11:47 +0900

    EventController에 이벤트 참여 api 추가

---
+++ src/main/java/com/kkumteul/domain/event/controller/EventController.java
@@ -0,0 +1,23 @@
+package com.kkumteul.domain.event.controller;
+
+
+import com.kkumteul.domain.event.dto.EventRequestDto;
+import com.kkumteul.domain.event.service.EventService;
+import com.kkumteul.util.ApiUtil;
+import com.kkumteul.util.ApiUtil.ApiSuccess;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/events")
+public class EventController {
+    private final EventService eventService;
+
+    @PostMapping("{userId}")
+    public ApiSuccess<?> getUser(@PathVariable(name = "userId") Long userId, @RequestBody EventRequestDto eventRequestDto) {
+        // TODO: userId JWT 방식으로 변경
+        eventService.insertJoinEvent(userId, eventRequestDto);
+        return ApiUtil.success("joined event successfully");
+    }
+}
