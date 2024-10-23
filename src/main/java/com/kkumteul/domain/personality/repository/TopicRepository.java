b3f1ac718719f63f471dd35eeafa2c351b848a4f
xnfnfnr <hjs000914@gmail.com>
2024-10-23 10:21:49 +0900

    Feat: TopicRepository 메소드 변경 insertTopic >> findByTopic으로 변경

--- src/main/java/com/kkumteul/domain/personality/repository/TopicRepository.java
+++ src/main/java/com/kkumteul/domain/personality/repository/TopicRepository.java
@@ -1,10 +1,11 @@
 package com.kkumteul.domain.personality.repository;
 
-import com.kkumteul.domain.personality.entity.Genre;
 import com.kkumteul.domain.personality.entity.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TopicRepository extends JpaRepository<Topic, Long> {
+
+    Topic findByTopic(String topic);
 }
