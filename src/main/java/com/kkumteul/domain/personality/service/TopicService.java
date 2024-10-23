a8e766cb8e315d1a3f5ae4f66c77336a22e53a80
xnfnfnr <hjs000914@gmail.com>
2024-10-23 10:25:18 +0900

    Feat: 레포지토리 변경에 따른 TopicService 내용 변경 insert >> get

--- src/main/java/com/kkumteul/domain/personality/service/TopicService.java
+++ src/main/java/com/kkumteul/domain/personality/service/TopicService.java
@@ -3,21 +3,20 @@ package com.kkumteul.domain.personality.service;
 import com.kkumteul.domain.personality.entity.Topic;
 import com.kkumteul.domain.personality.repository.TopicRepository;
 import jakarta.transaction.Transactional;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-//@RequiredArgsConstructor
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Transactional
 public class TopicService {
 
-    private TopicRepository topicRepository;
+    private final TopicRepository topicRepository;
 
-    // 1. 주제어 등록
-    public Topic insertTopic(Topic topic) {
-        return topicRepository.save(topic);
+    // 1. 주제어 이름으로 주제어 객체 가져오기
+    public Topic getTopic(String topic) {
+        return topicRepository.findByTopic(topic);
     }
 }
