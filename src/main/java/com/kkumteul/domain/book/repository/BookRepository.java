49a2809a2c50660bad8a569479699ad2d4c8a740
xnfnfnr <hjs000914@gmail.com>
2024-10-24 17:10:34 +0900

    Feat: 전체도서조회 레포지토리 구현 민아님 코드와 동일. Pageable과 JPQL로 구현

--- src/main/java/com/kkumteul/domain/book/repository/BookRepository.java
+++ src/main/java/com/kkumteul/domain/book/repository/BookRepository.java
@@ -1,9 +1,21 @@
 package com.kkumteul.domain.book.repository;
 
 import com.kkumteul.domain.book.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    // 민아님 코드와 동일
+    @Query(value = """
+                SELECT b
+                  FROM Book b
+                  JOIN FETCH b.bookTopics bt
+                  JOIN FETCH bt.topic t
+                 ORDER BY b.id ASC
+            """)
+    Page<Book> findAllBookInfo(final Pageable pageable);
 }
