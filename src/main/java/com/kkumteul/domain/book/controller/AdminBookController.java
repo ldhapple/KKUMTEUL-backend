025c07ace2f6ca1caf9781751d0af71fbbab3480
xnfnfnr <hjs000914@gmail.com>
2024-10-24 17:12:20 +0900

    Feat: 전체도서목록 및 상세 조회 API 호출 구현

--- src/main/java/com/kkumteul/domain/book/controller/AdminBookController.java
+++ src/main/java/com/kkumteul/domain/book/controller/AdminBookController.java
@@ -1,13 +1,18 @@
 package com.kkumteul.domain.book.controller;
 
+import com.kkumteul.domain.book.dto.AdminGetBookDetailResponseDto;
+import com.kkumteul.domain.book.dto.AdminGetBookListResponseDto;
 import com.kkumteul.domain.book.dto.AdminInsertBookRequestDto;
 import com.kkumteul.domain.book.service.BookService;
 import com.kkumteul.util.ApiUtil;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 import static com.kkumteul.util.ApiUtil.ApiSuccess;
 
@@ -19,14 +24,32 @@ public class AdminBookController {
 
     private final BookService bookService;
 
-    // 1. 도서 등록
+    // 1. 관리자의 도서 등록
     @PostMapping
     public ApiSuccess<?> insertBook(
             @RequestPart(value = "image") MultipartFile image,
-            @RequestPart(value = "book") AdminInsertBookRequestDto adminInsertBookRequestDto
-    ) {
+            @RequestPart(value = "book") AdminInsertBookRequestDto adminInsertBookRequestDto) {
+
         bookService.insertBook(adminInsertBookRequestDto, image);
 
         return ApiUtil.success("book insert successfully");
+    }
+
+    // 2. 관리자의 전체 도서 목록 조회
+    @GetMapping
+    public ApiSuccess<?> getAdminBookList(final Pageable pageable) {
+
+        Page<AdminGetBookListResponseDto> bookList = bookService.getAdminBookList(pageable);
+
+        return ApiUtil.success(bookList);
+    }
+
+    // 3. 관리자의 도서 상세 조회
+    @GetMapping("/{bookId}")
+    public ApiSuccess<?> getAdminBookDetail(@PathVariable("bookId") final Long bookId) {
+
+        AdminGetBookDetailResponseDto bookDetail = bookService.getBookDetailById(bookId);
+
+        return ApiUtil.success(bookDetail);
     }
 }
--- src/main/java/com/kkumteul/domain/book/service/BookMBTIService.java
+++ src/main/java/com/kkumteul/domain/book/service/BookMBTIService.java
@@ -3,7 +3,6 @@ package com.kkumteul.domain.book.service;
 import com.kkumteul.domain.book.entity.BookMBTI;
 import com.kkumteul.domain.book.repository.BookMBTIRepository;
 import jakarta.transaction.Transactional;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
