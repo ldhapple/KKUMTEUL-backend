d162e95848f0d52b172b9b96b02e1b508998d94d
xnfnfnr <hjs000914@gmail.com>
2024-10-24 17:02:30 +0900

    Feat: 도서 상세 조회 응답 Dto 구현

--- src/main/java/com/kkumteul/domain/book/dto/AdminGetBookDetailResponseDto.java
+++ src/main/java/com/kkumteul/domain/book/dto/AdminGetBookDetailResponseDto.java
@@ -1,5 +1,13 @@
 package com.kkumteul.domain.book.dto;
 
+import com.kkumteul.domain.book.entity.Book;
+import com.kkumteul.domain.book.entity.BookMBTI;
+import com.kkumteul.domain.book.entity.BookTopic;
+import com.kkumteul.domain.book.service.BookMBTIService;
+import com.kkumteul.domain.mbti.service.MBTIService;
+import com.kkumteul.domain.personality.entity.Genre;
+import com.kkumteul.domain.personality.entity.Topic;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,13 +15,39 @@ import java.util.List;
 
 @Getter
 @AllArgsConstructor
-public class AdminGetBookListResponseDto {
+public class AdminGetBookDetailResponseDto {
     private Long id;
+    private byte[] image;
     private String title;
-    private String author;
     private String publisher;
-    private String bookGenre;
+    private String author;
+    private String price;
     private String ageGroup;
+    private String bookGenre;
     private List<String> bookTopicList;
     private String bookMBTI;
+    private String summary;
+    private String page;
+
+    public static AdminGetBookDetailResponseDto fromEntity(final Book book) {
+        return new AdminGetBookDetailResponseDto(
+                book.getId(),
+                book.getBookImage(),
+                book.getTitle(),
+                book.getPublisher(),
+                book.getAuthor(),
+                book.getPrice(),
+                book.getAgeGroup(),
+                book.getGenre().getName(),
+                book.getBookTopics().stream()
+                        .map(BookTopic::getTopic)
+                        .map(Topic::getName)
+                        .toList(),
+                book.getBookMBTIs().get(0)
+                        .getMbti()
+                        .getTitle(),
+                book.getSummary(),
+                book.getPage()
+        );
+    }
 }
