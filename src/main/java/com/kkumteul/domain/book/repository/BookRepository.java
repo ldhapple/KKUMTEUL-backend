2e7aca6a5bbe39b528bd782706670a6b4d242fbd
leedohyun <lucky77795@naver.com>
2024-10-25 15:58:09 +0900

    Feat: 각 Entity 조회 시 관련 데이터를 한 번에 가져오기 위한 쿼리 작성
    성향 변경에 사용되는 Entity들 중 관련 목록을 같이 가져와야 하는 부분 추가.

--- src/main/java/com/kkumteul/domain/book/repository/BookRepository.java
+++ src/main/java/com/kkumteul/domain/book/repository/BookRepository.java
@@ -1,10 +1,12 @@
 package com.kkumteul.domain.book.repository;
 
 import com.kkumteul.domain.book.entity.Book;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     @Query(value = """
@@ -15,4 +17,7 @@ public interface BookRepository extends JpaRepository<Book, Long> {
                  ORDER BY b.id ASC
             """)
     Page<Book> findAllBookInfo(final Pageable pageable);
+
+    @Query("SELECT b FROM Book b JOIN FETCH b.genre JOIN FETCH b.bookTopics WHERE b.id = :bookId")
+    Optional<Book> findBookByIdWithGenreAndTopic(@Param("bookId") Long bookId);
 }
