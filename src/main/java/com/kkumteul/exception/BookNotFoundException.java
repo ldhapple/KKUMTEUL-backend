9d4bbac326323123fff2b9a76e3874e19226f400
xnfnfnr <hjs000914@gmail.com>
2024-10-25 14:39:06 +0900

    Refactor: 예외처리 파라미터 타입 변경

--- src/main/java/com/kkumteul/exception/BookNotFoundException.java
+++ src/main/java/com/kkumteul/exception/BookNotFoundException.java
@@ -1,7 +1,7 @@
 package com.kkumteul.exception;
 
 public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(Long input) {
-        super(String.format("해당 도서를 찾을 수 없습니다. InputBookId: %s", input));
+    public BookNotFoundException(String input) {
+        super("해당 도서를 찾을 수 없습니다. InputBookId: " + input);
     }
 }
