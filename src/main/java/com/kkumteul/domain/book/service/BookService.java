5fccd5a67f330a3c41d50e41d05d9705526b0bfc
xnfnfnr <hjs000914@gmail.com>
2024-10-28 14:32:06 +0900

    Refactor: 사용하지 않은 import 제거

--- src/main/java/com/kkumteul/domain/book/service/BookService.java
+++ src/main/java/com/kkumteul/domain/book/service/BookService.java
@@ -16,14 +16,12 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
