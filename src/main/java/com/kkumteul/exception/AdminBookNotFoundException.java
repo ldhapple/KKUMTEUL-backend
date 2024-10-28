b7c0f791686f983875fe2f9dec0bd50f9a08039e
Ha Jin Seo <131326799+xnfnfnr@users.noreply.github.com>
2024-10-28 17:34:51 +0900

    Refactor: Service 클래스 이름 변경 BookService >> AdminBookService

--- src/main/java/com/kkumteul/exception/AdminBookNotFoundException.java
+++ src/main/java/com/kkumteul/exception/AdminBookNotFoundException.java
@@ -1,7 +1,7 @@
 package com.kkumteul.exception;
 
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(String input) {
+public class AdminBookNotFoundException extends RuntimeException {
+    public AdminBookNotFoundException(String input) {
         super("해당 도서를 찾을 수 없습니다. InputBookId: " + input);
     }
 }
