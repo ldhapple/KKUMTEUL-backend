e9e81c302c3f5f55ea3a5c66dab3e971d93ecd1b
hh830 <jhh8890@naver.com>
2024-10-28 18:43:34 +0900

    Test: 추천 도서 조회 컨트롤러 테스트

--- src/test/java/com/kkumteul/recommendation/controller/RecommendationControllerTest.java
+++ src/test/java/com/kkumteul/recommendation/controller/RecommendationControllerTest.java
@@ -1,5 +1,6 @@
 package com.kkumteul.recommendation.controller;
 
+import com.kkumteul.domain.recommendation.controller.RecommendationController;
 import com.kkumteul.domain.recommendation.service.RecommendationService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.nio.charset.StandardCharsets;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -20,17 +23,16 @@ public class RecommendationControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private RecommendationService recommendationService;
+
 
     @Test
-    @DisplayName("추천에 사용 될 데이터 가져오기")
+    @DisplayName("추천 도서 가져오기")
     void testData() throws Exception{
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/recommend/1"))
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/recommendation/books/1"))
                 .andExpect(status().isOk())
                 .andReturn();
         // 응답 본문을 문자열로 추출
-        String content = result.getResponse().getContentAsString();
+        String content = result.getResponse().getContentAsString(StandardCharsets.UTF_8);
 
         // 응답 본문 출력
         System.out.println("Response: " + content);
