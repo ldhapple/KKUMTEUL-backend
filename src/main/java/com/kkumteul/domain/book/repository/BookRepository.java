83b30fa3476a40759803e9f332e58a1fb2450e30
이도현 <lucky77795@naver.com>
2024-10-30 14:21:32 +0900

    Refactor: 페치 조인 시 도서 목록 조회가 느렸던 부분 수정
    BatchSize를 이용해 N+1 문제를 예방하고 속도를 개선

--- src/main/java/com/kkumteul/domain/book/repository/BookRepository.java
+++ src/main/java/com/kkumteul/domain/book/repository/BookRepository.java
@@ -19,8 +19,6 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     @Query(value = """
                 SELECT b
                   FROM Book b
-                  JOIN FETCH b.bookTopics bt
-                  JOIN FETCH bt.topic t
                  ORDER BY b.id ASC
             """)
     Page<Book> findAllBookInfo(final Pageable pageable);
