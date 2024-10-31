806bab96e311c6db72b6138c69248b3b145b99c5
MINAH <tjdns7831@naver.com>
2024-10-31 17:43:20 +0900

    refactor: 주석 제거

--- src/main/java/com/kkumteul/domain/book/controller/BookController.java
+++ src/main/java/com/kkumteul/domain/book/controller/BookController.java
@@ -11,7 +11,6 @@ import com.kkumteul.util.ApiUtil.ApiSuccess;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
