391cd1efcbb13989268f08fd676492fc59e92b50
hh830 <jhh8890@naver.com>
2024-11-02 17:39:56 +0900

    Test: 기본 추천 로직 테스트 추가

--- src/test/java/com/kkumteul/domain/recommendation/service/RecommendationServiceTest.java
+++ src/test/java/com/kkumteul/domain/recommendation/service/RecommendationServiceTest.java
@@ -1,11 +1,17 @@
 package com.kkumteul.domain.recommendation.service;
 
+import static com.kkumteul.domain.childprofile.entity.Gender.MALE;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.kkumteul.domain.book.entity.Book;
+import com.kkumteul.domain.book.repository.BookRepository;
+import com.kkumteul.domain.childprofile.entity.ChildProfile;
 import com.kkumteul.domain.recommendation.dto.RecommendBookDto;
+import com.kkumteul.domain.recommendation.entity.Recommendation;
 import com.kkumteul.domain.recommendation.repository.RecommendationRepository;
 import com.kkumteul.exception.RecommendationBookNotFoundException;
 import java.util.List;
@@ -16,6 +22,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @ExtendWith(MockitoExtension.class)
 class RecommendationServiceTest {
@@ -25,6 +33,9 @@ class RecommendationServiceTest {
 
     @InjectMocks
     private RecommendationService recommendationService;
+
+    @Mock
+    private BookRepository bookRepository;
 
     @Test
     @DisplayName("자녀 프로필에 따른 추천 도서 가져오기 성공 테스트")
@@ -63,4 +74,24 @@ class RecommendationServiceTest {
                 recommendationService.getRecommendedBooks(childProfileId)
         );
     }
+
+    @Test
+    @DisplayName("기본 추천 로직 테스트")
+    void testDefaultRecommend() {
+        // 테스트 데이터 설정 및 DB 삽입
+        Book book1 = new Book("Title1", "Author", "Publisher", "Price", "Page", "4세부터", "Summary", new byte[]{}, null, null, null);
+        Book book2 = new Book("Title2", "Author", "Publisher", "Price", "Page", "5세부터", "Summary", new byte[]{}, null, null, null);
+
+        Pageable pageable = PageRequest.of(0, 20);
+        List<Book> books = List.of(book2, book1);
+
+        given(bookRepository.findBookListByAgeGroup(10, pageable)).willReturn(books);
+
+        List<Book> results = recommendationService.getDefaultRecommendations(10);
+
+        // 10살이니까 book2가 더 위에 있음
+        assertEquals("Title2", results.get(0).getTitle());
+        assertEquals("Title1", results.get(1).getTitle());
+    }
+
 }
