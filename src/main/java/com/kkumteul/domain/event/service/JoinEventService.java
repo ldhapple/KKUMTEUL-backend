d9824db14857e55ccfcd4effef504fa04b11c1dc
이도현 <lucky77795@naver.com>
2024-11-02 23:17:02 +0900

    Feat: 응모 시 유효한 티켓을 가지고 있는 지 검증하는 로직 추가.
    티켓 사용 시 활성화 티켓에서 삭제

--- src/main/java/com/kkumteul/domain/event/service/JoinEventService.java
+++ src/main/java/com/kkumteul/domain/event/service/JoinEventService.java
@@ -26,6 +26,8 @@ public class JoinEventService {
 
     public String joinEvent(Long userId, JoinEventRequestDto joinEventRequestDto) {
         String winnersSetKey = "winners"; // 이름, 전화번호가 요청되었을때 저장할 set
+        String activeTicketsKey = "active_tickets";
+        String ticketKey = joinEventRequestDto.getTicket();
 
         // 이름 전화번호 중복 체크 (1:jung:01000000000:1)
         String winnerEntry = userId + ":" + joinEventRequestDto.getName() + ":" + joinEventRequestDto.getPhoneNumber() + ":" + joinEventRequestDto.getEventId();
@@ -36,9 +38,17 @@ public class JoinEventService {
             return "이미 해당 이름과 전화번호로 이벤트에 참여함";
         }
 
+        Boolean isTicketValid = template.opsForSet().isMember(activeTicketsKey, ticketKey);
+
+        if (Boolean.FALSE.equals(isTicketValid)) {
+            return "세션이 만료되었습니다. 다시 응모해주세요!";
+        }
+
         // 이름과 전화번호가 입력되었을 경우 winners 에 저장
         template.opsForSet().add(winnersSetKey, winnerEntry);
+        template.opsForSet().remove(activeTicketsKey, ticketKey);
         log.info("이벤트 참여 완료: " + winnerEntry);
+
         return "이름, 전화번호까지 입력해서 이벤트 참여 완료!!";
     }
 
