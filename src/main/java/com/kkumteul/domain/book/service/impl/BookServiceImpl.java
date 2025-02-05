06c3fefadb8805741b5883f988bd6dc459f158eb
이도현 <lucky77795@naver.com>
2025-02-05 20:28:23 +0900

    Refactor: 도서 검색 쿼리 개선

--- src/main/java/com/kkumteul/domain/book/service/impl/BookServiceImpl.java
+++ src/main/java/com/kkumteul/domain/book/service/impl/BookServiceImpl.java
@@ -12,9 +12,17 @@ import com.kkumteul.domain.book.exception.BookNotFoundException;
 import com.kkumteul.domain.book.repository.BookLikeRepository;
 import com.kkumteul.domain.book.repository.BookRepository;
 import com.kkumteul.domain.book.service.BookService;
+import com.kkumteul.domain.personality.repository.TopicRepository;
 import com.kkumteul.exception.EntityNotFoundException;
 import com.kkumteul.domain.childprofile.repository.ChildProfileRepository;
 import com.kkumteul.util.kafka.KafkaUtil;
+import jakarta.persistence.Tuple;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
@@ -37,6 +45,7 @@ public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
     private final BookLikeRepository bookLikeRepository;
     private final ChildProfileRepository childProfileRepository;
+    private final TopicRepository topicRepository;
     private final KafkaUtil kafkaUtil;
 
     // 전체 도서 목록 조회
@@ -64,12 +73,23 @@ public class BookServiceImpl implements BookService {
         return bookRepository.findBookByIdWithGenreAndTopic(bookId)
                 .orElseThrow(() -> new EntityNotFoundException(bookId));
     }
+
     // 검색 키워드 추출된 도서 목록 조회
     @Override
     public Page<GetBookListResponseDto> getBookList(final String keyword, final Pageable pageable) {
-        final Page<Book> books = bookRepository.findBookListByKeyword(keyword, pageable);
+//        final Page<Book> books = bookRepository.findBookListByKeyword(keyword, pageable);
+//        Page<Tuple> books = bookRepository.findBookListByKeyword(keyword, pageable);
+//        return books.map(GetBookListResponseDto::create);
+        List<String> validTopics = topicRepository.findAllTopicNames();
+        Set<String> topics = new HashSet<>(validTopics);
 
-        return books.map(GetBookListResponseDto::from);
+        Page<Tuple> results = bookRepository.findBookListByKeyword(keyword, pageable);
+
+        return results.map(result -> {
+            List<String> topicNames = extractTopics(result.get("topicNames", String.class), topics);
+            return GetBookListResponseDto.create(result, topicNames);
+        });
+//        return books.map(GetBookListResponseDto::from);
     }
 
     // 상세 도서 조회
@@ -101,14 +121,16 @@ public class BookServiceImpl implements BookService {
             if (bookLike.getLikeType() == likeType) {
                 throw new RuntimeException(likeType == LikeType.LIKE ? "이미 좋아요를 눌렀습니다." : "이미 싫어요를 눌렀습니다.");
             } else {
-            // 2-2. 상태 변경
+                // 2-2. 상태 변경
                 bookLike.updateLikeType(likeType);
                 bookLike.updateUpdateAt();
 
                 bookLikeRepository.save(bookLike);
 
-                kafkaUtil.sendMessage(likeType == LikeType.LIKE ? BOOK_LIKE.getTopicName() : BOOK_DISLIKE.getTopicName(), message);
-                log.info("Kafka message sent - childProfileId: {}, bookId: {}, likeType: {}", childProfileId, bookId, likeType);
+                kafkaUtil.sendMessage(
+                        likeType == LikeType.LIKE ? BOOK_LIKE.getTopicName() : BOOK_DISLIKE.getTopicName(), message);
+                log.info("Kafka message sent - childProfileId: {}, bookId: {}, likeType: {}", childProfileId, bookId,
+                        likeType);
             }
         } else {
             // 3. 처음 누를 때
@@ -122,8 +144,10 @@ public class BookServiceImpl implements BookService {
 
             bookLikeRepository.save(booklike);
 
-            kafkaUtil.sendMessage(likeType == LikeType.LIKE ? BOOK_LIKE.getTopicName() : BOOK_DISLIKE.getTopicName(), message);
-            log.info("Kafka message sent - childProfileId: {}, bookId: {}, likeType: {}", childProfileId, bookId, likeType);
+            kafkaUtil.sendMessage(likeType == LikeType.LIKE ? BOOK_LIKE.getTopicName() : BOOK_DISLIKE.getTopicName(),
+                    message);
+            log.info("Kafka message sent - childProfileId: {}, bookId: {}, likeType: {}", childProfileId, bookId,
+                    likeType);
         }
     }
 
@@ -132,13 +156,25 @@ public class BookServiceImpl implements BookService {
         Map<String, Boolean> likeStatus = new HashMap<>();
 
         // 좋아요 상태 확인
-        boolean isLiked = bookLikeRepository.existsByBookIdAndChildProfileIdAndLikeType(bookId, childProfileId, LikeType.LIKE);
+        boolean isLiked = bookLikeRepository.existsByBookIdAndChildProfileIdAndLikeType(bookId, childProfileId,
+                LikeType.LIKE);
         likeStatus.put("isLiked", isLiked);
 
         // 싫어요 상태 확인
-        boolean isDisliked = bookLikeRepository.existsByBookIdAndChildProfileIdAndLikeType(bookId, childProfileId, LikeType.DISLIKE);
+        boolean isDisliked = bookLikeRepository.existsByBookIdAndChildProfileIdAndLikeType(bookId, childProfileId,
+                LikeType.DISLIKE);
         likeStatus.put("isDisliked", isDisliked);
 
         return likeStatus;
+    }
+
+    private List<String> extractTopics(String searchText, Set<String> topics) {
+        if (searchText == null || searchText.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        return Arrays.stream(searchText.split(" "))
+                .filter(topics::contains)
+                .collect(Collectors.toList());
     }
 }
